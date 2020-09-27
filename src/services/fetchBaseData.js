3a0a1c48d44e3059e8c1ec2406a03a55e3ff0f65
arabinelli <andrea.rabinelli@gmail.com>
2020-09-27 22:33:03 +0200

    increase logging level

--- src/services/fetchBaseData.js
+++ src/services/fetchBaseData.js
@@ -8,6 +8,7 @@ const fetchAPI = async (url, payload) => {
 };
 
 async function fetchGraphData(hashtags, language, setError) {
+  console.log(backendBaseUrl);
   const url = backendBaseUrl + "/get-graph";
   const requestBody = {
     hashtags: hashtags.split(" "),
