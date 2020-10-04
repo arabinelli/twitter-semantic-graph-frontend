dbed5c8c908603a97c02da43831435ec80057a3c
arabinelli <andrea.rabinelli@gmail.com>
2020-10-04 16:42:36 +0200

    refactor: init analytics only after consent

--- src/services/analytics.js
+++ src/services/analytics.js
@@ -1,15 +1,13 @@
-import React from "react";
 import ReactGA, { ga } from "react-ga";
 import Cookies from "js-cookie";
 
 const trackUsage = () => {
-  ReactGA.initialize("UA-123384350-2");
-  ReactGA.set({ anonymizeIp: true });
-
   let gaConsent = Cookies.get("rcl_statistics_consent");
   console.log(gaConsent);
 
   if (gaConsent) {
+    ReactGA.initialize("UA-123384350-2");
+    ReactGA.set({ anonymizeIp: true });
     ReactGA.pageview(window.location.pathname + window.location.search);
   }
 };
